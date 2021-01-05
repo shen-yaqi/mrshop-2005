a67f0ffeffc397a1e4f1e8022f1cb955ffcb1c6b
shen-yaqi <shenyaqiii@163.com>
2021-01-05 17:54:34 +0800

    上传服务集成fastdfs 商品查询1.0

---
+++ mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/service/GoodsService.java
@@ -0,0 +1,26 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SpuDTO;
+import com.baidu.shop.entity.SpuEntity;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * @ClassName GoodsService
+ * @Description: TODO
+ * @Author shenyaqi
+ * @Date 2021/1/5
+ * @Version V1.0
+ **/
+@Api(tags = "商品接口")
+public interface GoodsService {
+
+    @ApiOperation(value = "查询spu信息")
+    @GetMapping(value = "/goods/getSpuInfo")
+    Result<PageInfo<SpuEntity>> getSpuInfo(SpuDTO spuDTO);
+}
