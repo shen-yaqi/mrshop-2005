b20d4bc855fe4b4453890ea238704fa0daa1956e
shen-yaqi <shenyaqiii@163.com>
2021-03-11 17:57:54 +0800

    验证短信验证码-注册2

---
+++ mingrui-shop-parent/mingrui-shop-commons/mingrui-shop-common-core/src/main/java/com/baidu/shop/constant/MrConstants.java
@@ -0,0 +1,13 @@
+package com.baidu.shop.constant;
+
+/**
+ * @ClassName MrConstants
+ * @Description: TODO
+ * @Author shenyaqi
+ * @Date 2021/3/11
+ * @Version V1.0
+ **/
+public class MrConstants {
+
+    public static final String REDIS_DUANXIN_CODE_PRE = "valid-code-";
+}
