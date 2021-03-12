2144f56effc42b4b74c3e0d45689dd89754cb5d6
shen-yaqi <shenyaqiii@163.com>
2021-03-12 17:52:13 +0800

    验证用户登录

--- mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-basic-user-oauth/src/main/java/com/baidu/shop/web/UserOauthController.java
+++ mingrui-shop-parent/mingrui-shop-basics/mingrui-shop-basic-user-oauth/src/main/java/com/baidu/shop/web/UserOauthController.java
@@ -4,15 +4,15 @@ import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.business.UserOauthService;
 import com.baidu.shop.config.JwtConfig;
+import com.baidu.shop.dto.UserInfo;
 import com.baidu.shop.entity.UserEntity;
+import com.baidu.shop.status.HTTPStatus;
 import com.baidu.shop.utils.CookieUtils;
+import com.baidu.shop.utils.JwtUtils;
 import com.baidu.shop.utils.ObjectUtil;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +33,23 @@ public class UserOauthController extends BaseApiService {
     private UserOauthService userOauthService;
     @Autowired
     private JwtConfig jwtConfig;
+
+    @GetMapping(value = "verify")
+    public Result<UserInfo> checkUserIsLogin(@CookieValue(value = "MRSHOP_TOKEN") String token
+            , HttpServletRequest request, HttpServletResponse response){
+        UserInfo userInfo = null;
+        try {
+            userInfo = JwtUtils.getInfoFromToken(token, jwtConfig.getPublicKey());
+
+            String newToken = JwtUtils.generateToken(userInfo, jwtConfig.getPrivateKey(), jwtConfig.getExpire());
+            CookieUtils.setCookie(request,response,jwtConfig.getCookieName(),newToken,jwtConfig.getCookieMaxAge(),true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return this.setResultError(HTTPStatus.USER_NOT_LOGIN,"用户没有登录");
+        }
+        return this.setResultSuccess(userInfo);
+    }
 
     @PostMapping(value = "login")
     public Result<JSONObject> login(@RequestBody UserEntity userEntity, HttpServletRequest request, HttpServletResponse response){
--- mingrui-shop-parent/mingrui-shop-commons/mingrui-shop-common-core/src/main/java/com/baidu/shop/status/HTTPStatus.java
+++ mingrui-shop-parent/mingrui-shop-commons/mingrui-shop-common-core/src/main/java/com/baidu/shop/status/HTTPStatus.java
@@ -16,4 +16,6 @@ public class HTTPStatus {
     public static final int OPERATION_ERROR = 5001;//失败
 
     public static final int PARAMS_VALIDATE_ERROR = 5002;
+
+    public static final int USER_NOT_LOGIN = 403;
 }
